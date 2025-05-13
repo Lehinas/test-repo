bfc18e58600bb7c369a8eef6bfdf81b934830b4a
Lehinas <lehinas201283@mail.ru>
2025-05-13 18:54:49 +0500

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT ? process.env.PORT : 3001
+app.get("/random", (req, res) => {
+		const num = Math.floor(Math.random() * 5) + 1
+
+		setTimeout(() => {
+				res.json({number: num})
+		}, num * 1000)
+})
+
+app.listen(PORT, () => {
+		console.log(`Сервер запущен на http://localhost:${PORT}`)
+})

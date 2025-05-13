12f27553579192005ad4a834a7b770253c919f3c
Lehinas <lehinas201283@mail.ru>
2025-05-13 19:30:59 +0500

    fix cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const PORT = process.env.PORT ? process.env.PORT : 3001
+
+app.use(cors())
+
 app.get("/random", (req, res) => {
 		const num = Math.floor(Math.random() * 5) + 1
 
